ecf858c41125c4434bad084c2a72b86b21ef60e4
sheikh-zafar <audios.shkzafar@gmail.com>
2023-02-28 20:18:09 +0400

    commit

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import '@/styles/globals.css';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+
+const clientSideEmotionCache = createCache({ key: 'css' });
+
+export default function App({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
+}
---
+++ pages/_document.js
@@ -0,0 +1,47 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import createCache from '@emotion/cache';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+  const cache = createCache({ key: 'css' });
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+  ctx.renderPage = () =>
+    originalRenderPage({
+      // eslint-disable-next-line react/display-name
+      enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
+    });
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
